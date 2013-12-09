2663c18012885571f38d4ecb9c75deaf9dc6315e
hiearth <hiearth@example.com>
2013-12-09 14:06:43 +0800

    refactor character repository

--- langstyle/database/base_repository.py
+++ langstyle/database/base_repository.py
@@ -10,3 +10,38 @@ class BaseRepository:
 
     def _log_error(self, error):
         config.service_factory.get_log_service().error(error.msg)
+
+    def _fetch_one(self,cursor):
+        for result in cursor.stored_results():
+            first_result = result.fetchone()
+            if first_result:
+                return first_result[0]
+        return None
+
+    def _call_proc_fetch_one(self, proc_name, proc_args):
+        try:
+            conn = self._create_connection()
+            cursor = conn.cursor()
+            cursor.callproc(proc_name, proc_args)
+            return self._fetch_one(cursor)
+        except dbconnector.DatabaseError as db_error:
+            self._log_error(db_error)
+        finally:
+            cursor.close()
+            conn.close()
+        return None
+
+    def _call_proc_update(self, proc_name, proc_args):
+        try:
+            conn = self._create_connection()
+            cursor = conn.cursor()
+            result = cursor.callproc(proc_name,proc_args)
+            conn.commit()
+            return result
+        except dbconnector.DatabaseError as db_error:
+            conn.rollback()
+            self._log_error(db_error)
+        finally:
+            cursor.close()
+            conn.close()
+        return None
--- langstyle/database/character_repository.py
+++ langstyle/database/character_repository.py
@@ -6,46 +6,13 @@ from . import base_repository
 class CharacterRepository(base_repository.BaseRepository):
 
     def add(self, word_character):
-        try:
-            conn = self._create_connection()
-            cursor = conn.cursor()
-            result = cursor.callproc("WordCharacter_I",(word_character, None))
+        result = self._call_proc_update("WordCharacter_I",(word_character, None))
+        if result:
             return result[1]
-        except dbconnector.DatabaseError as db_error:
-            self._log_error(db_error)
-        finally:
-            cursor.close()
-            conn.commit()
-            conn.close()
         return None
 
     def get(self, character_id):
-        try:
-            conn = self._create_connection()
-            cursor = conn.cursor()
-            cursor.callproc("WordCharacter_S_By_Id", [character_id])
-            return self._fetch_one(cursor)
-        except dbconnector.DatabaseError as db_error:
-            self._log_error(db_error)
-        finally:
-            cursor.close()
-            conn.close()
+        return self._call_proc_fetch_one("WordCharacter_S_By_Id", [character_id])
 
     def get_id(self, word_character):
-        try:
-            conn = self._create_connection()
-            cursor = conn.cursor()
-            cursor.callproc("WordCharacter_S_By_Code", [word_character])
-            return self._fetch_one(cursor)
-        except dbconnector.DatabaseError as db_error:
-            self._log_error(db_error)
-        finally:
-            cursor.close()
-            conn.close()
-
-    def _fetch_one(self,cursor):
-        for result in cursor.stored_results():
-            first_result = result.fetchone()
-            if first_result:
-                return first_result[0]
-        return None
+        return self._call_proc_fetch_one("WordCharacter_S_By_Code", [word_character])
