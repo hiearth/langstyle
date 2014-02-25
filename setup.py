e9ccef33ee84e4769b08b076fe86ba22ab99015f
hiearth <hiearth@example.com>
2014-02-25 11:06:14 +0800

    rename run_server to app

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='langstyle',
       version='1.0',
       description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
-      url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
+      author='hiearth',
+      author_email='hiearth.hj@gmail.com',
+      url='http://langstyle-hiearth.rhcloud.com/'
      )
