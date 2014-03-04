8a06929af339253fa0ca37716b5c6cb74be1af5a
hiearth <hiearth@example.com>
2014-03-04 17:43:27 +0800

    database upgrade

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import langstyle.server
 import langstyle.install.database
 
 #upgrade database
-langstyle.install.database.drop_and_create()
+#langstyle.install.database.drop_and_create()
+langstyle.install.database.upgrade()
 
 langstyle.server.start()
--- langstyle/install/database.py
+++ langstyle/install/database.py
@@ -18,7 +18,29 @@ def _log_error(error_msg):
 def _log_debug(msg):
     config.service_factory.get_log_service().debug(msg)
 
-def _execute_command(cmd_text):
+def _fetch_one(cursor):
+    for result in cursor:
+        if result:
+            return result
+    return None
+
+def _execute_query_one(cmd_text):
+    conn = None
+    cursor = None
+    try:
+        conn = dbconnector.connect(**config.database_connection)
+        cursor = conn.cursor()
+        cursor.execute(cmd_text)
+        return _fetch_one(cursor)
+    except dbconnector.DatabaseError as db_error:
+        _log_error(db_error.msg)
+    finally:
+        if cursor:
+            cursor.close()
+        if conn:
+            conn.close()
+
+def _execute_non_query(cmd_text):
     conn = None
     cursor = None
     try:
@@ -40,9 +62,7 @@ def _execute_command(cmd_text):
 
 file_name_regex = re.compile(r"(?i)^([0-9]){1}\.([0-9]){1}\.([0-9]){1}\-(.*)\.sql$")
 
-def _file_compare(first_file_name, second_file_name):
-    first_file_pattern = file_name_regex.findall(first_file_name)[0]
-    second_file_pattern = file_name_regex.findall(second_file_name)[0]
+def _file_number_compare(first_file_pattern, second_file_pattern):
     if first_file_pattern[0] > second_file_pattern[0]:
         return 1
     elif first_file_pattern[0] < second_file_pattern[0]:
@@ -56,6 +76,16 @@ def _file_compare(first_file_name, second_file_name):
     elif first_file_pattern[2] < second_file_pattern[2]:
         return -1
     return 0
+
+def _get_file_number_pattern(file_pattern):
+    return (int(file_pattern[0]), int(file_pattern[1]), int(file_pattern[2]))
+
+def _file_compare(first_file_name, second_file_name):
+    first_file_pattern = file_name_regex.findall(first_file_name)[0]
+    second_file_pattern = file_name_regex.findall(second_file_name)[0]
+    first_number_pattern = _get_file_number_pattern(first_file_pattern)
+    second_number_pattern = _get_file_number_pattern(second_file_pattern)
+    return _file_number_compare(first_number_pattern, second_number_pattern)
 
 def _find_insert_index(sorted, file_name):
     for index in range(0, len(sorted)):
@@ -75,11 +105,14 @@ def _filter_file(file_name):
         return True
     return False
 
-def _get_schema_files(dir):
+def _get_schema_file_names(dir):
+    files = os.listdir(dir)
+    return [file_name for file_name in files if _filter_file(file_name)]
+
+def _get_sorted_schema_files(dir):
     try:
-        files = os.listdir(dir)
-        schema_files = [file_name for file_name in files if _filter_file(file_name)]
-        sorted_schema_files = _sort_files(schema_files)
+        schema_file_names = _get_schema_file_names(dir)
+        sorted_schema_files = _sort_files(schema_file_names)
         return [os.path.join(dir, file_name) for file_name in sorted_schema_files]
     except FileNotFoundError as e:
         _log_error("directory "+ dir + " not found")
@@ -88,12 +121,18 @@ def _read_file(full_file_path):
     with open(full_file_path, "r") as f:
         return " ".join(f.readlines())
 
+def _run_schema_files(file_paths):
+    for file_path in file_paths:
+        _run_schema_file(file_path)
+
+def _run_schema_file(file_path):
+    schema_content = _read_file(file_path)
+    _log_debug(file_path)
+    _execute_non_query(schema_content)
+
 def _create_schema():
-    schema_files = _get_schema_files(config.DATABASE_SCHEMA_DIR)
-    for file in schema_files:
-        schema_content = _read_file(file)
-        _log_debug(file)
-        _execute_command(schema_content)
+    schema_files = _get_sorted_schema_files(config.DATABASE_SCHEMA_DIR)
+    _run_schema_files(schema_files)
 
 def _get_procedure_files():
     dir = config.DATABASE_PROCEDURE_DIR
@@ -110,9 +149,30 @@ def _create_procedure():
         if std_error:
             _log_error(std_error)
 
-def create():
-    _create_schema()
-    _create_procedure()
+def _get_upgrade_files(max_number_pattern):
+    try:
+        schema_dir = config.DATABASE_SCHEMA_DIR
+        schema_file_names = _get_schema_file_names(schema_dir)
+        sorted_schema_files = _sort_files(schema_file_names)
+        for i in range(0, len(sorted_schema_files)):
+            file_pattern = file_name_regex.findall(sorted_schema_files[i])[0]
+            file_number_pattern = _get_file_number_pattern(file_pattern)
+            if _file_number_compare(file_number_pattern, max_number_pattern) > 0:
+                return sorted_schema_files[i:]
+    except FileNotFoundError as e:
+        _log_error("directory "+ schema_dir + " not found")
+    return []
+
+def _create_upgrade_schemas():
+    select_schema_changes = "select MajorReleaseNumber, MinorReleaseNumber, BuildNumber from SchemaChange order by MajorReleaseNumber desc, MinorReleaseNumber desc, BuildNumber desc limit 0, 1;"
+    max_db_schema = _execute_query_one(select_schema_changes)
+    if max_db_schema:
+        upgrade_files = _get_upgrade_files(max_db_schema)
+    else:
+        upgrade_files = _get_sorted_schema_files(config.DATABASE_SCHEMA_DIR)
+    if upgrade_files:
+        _log_debug("upgrade point: " + upgrade_files[0])
+    _run_schema_files([os.path.join(config.DATABASE_SCHEMA_DIR, file_name) for file_name in upgrade_files])
 
 def drop():
     db_name = config.database_connection.get("database")
@@ -120,11 +180,16 @@ def drop():
         _log_error("no database name to drop")
         return False
     drop_command = "drop database if exists " + db_name + ";"
-    return _execute_command(drop_command)
+    return _execute_non_query(drop_command)
+
+def create():
+    _create_schema()
+    _create_procedure()
+
+def upgrade():
+    _create_upgrade_schemas()
+    _create_procedure()
 
 def drop_and_create():
     if drop():
         create()
-
-def upgrade():
-    pass
